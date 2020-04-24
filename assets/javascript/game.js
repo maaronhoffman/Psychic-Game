a00e52e04325f772844c75667f6d8dc2450f41e4
Michael Hoffman <maaronhoffman@gmail.com>
2020-04-24 16:49:51 -0700

    Final Game I hope

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+var wins = 0;
+var totalLoss = 0;
+var alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var numWrongGuesses = 0;
+var cpuChoice = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+var userListing = [];
+var guessesDown = document.getElementById("guess");
+
+
+document.onkeyup = function makeChoice(event) {
+
+    var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+    userListing.push(userChoice);
+    document.getElementById("letters").textContent = "Letters: " + userListing;
+
+    if(userChoice == cpuChoice) {
+
+
+
+
+
+        wins++;
+        numWrongGuesses = 0;
+        userListing=[];
+        cpuChoice = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+        var targetScore = document.getElementById("scorewin");
+        targetScore.textContent = "Wins: " + wins;
+        alert("YOU WIN");
+
+
+
+
+    } 
+    
+    
+    
+    else {
+        
+        
+        
+        numWrongGuesses++;
+        guessesDown.textContent = "Guesses: " + (10 - numWrongGuesses);
+
+
+
+    }
+
+    if (numWrongGuesses === 10) 
+    
+    
+    
+    {
+        userListing = [];
+        totalLoss++;
+        numWrongGuesses = 0;
+        cpuChoice = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+        var targetLoss = document.getElementById("scoreloss");
+        targetLoss.textContent = "Loses: " + totalLoss;
+        alert("YOU LOSE");
+
+
+    }
+};
